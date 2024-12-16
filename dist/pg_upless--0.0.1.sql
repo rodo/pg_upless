594f5bb3911c04b2acfd305ed58e56f19813f000
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-12-16 12:03:50 +0100

    Prepare the first release

--- dist/pg_upless--0.0.1.sql
+++ dist/pg_upless--0.0.1.sql
@@ -32,11 +32,12 @@ LANGUAGE plpgsql AS
 $$
 DECLARE qry text;
 BEGIN
-
+    -- keep the time we set it up
     INSERT INTO @extschema@.pg_upless_start_time(relnamespace, relname, start_time)
     VALUES (schema_source, table_source, current_timestamp)
     ON CONFLICT (relnamespace, relname) DO NOTHING;
 
+    -- create the triggers
     SELECT pg_upless_create_trigger(schema_source, table_source) INTO qry;
     EXECUTE qry;
 
@@ -62,21 +63,25 @@ BEGIN
    RETURN format('Trigger dropped on %s.%s', schema_source, table_source);
 END;
 $$;
-
-
-
+--
+-- The function called by the trigger
+--
+-- Will update the stats if the record is changed or not
+--
 CREATE OR REPLACE FUNCTION pg_upless_stats_trg()
 RETURNS trigger
 LANGUAGE plpgsql AS
 $$
 
 BEGIN
-   IF NEW != OLD THEN
+   IF NOT pg_upless_compare_record(NEW, OLD) THEN
+       -- records are different
        INSERT INTO @extschema@.pg_upless_stats (relnamespace, relname, useful, useless)
        VALUES (TG_TABLE_SCHEMA, TG_TABLE_NAME, 1, 0)
        ON CONFLICT (relnamespace, relname) DO UPDATE
        SET useful = pg_upless_stats.useful + 1;
    ELSE
+       -- records are identical
        INSERT INTO @extschema@.pg_upless_stats (relnamespace, relname, useful, useless)
        VALUES (TG_TABLE_SCHEMA, TG_TABLE_NAME, 1, 0)
        ON CONFLICT (relnamespace, relname) DO UPDATE
@@ -84,6 +89,28 @@ BEGIN
     END IF;
 
     RETURN NEW;
+END;
+$$;
+--
+-- Compare two record
+--
+-- Return True if the two records are the same
+--
+CREATE OR REPLACE FUNCTION pg_upless_compare_record(new_r record, old_r record)
+RETURNS boolean
+LANGUAGE plpgsql AS
+$$
+DECLARE
+  colexclu text[];
+BEGIN
+   SELECT ARRAY(SELECT colname FROM @extschema@.pg_upless_column_exclusion) INTO colexclu;
+
+   IF to_jsonb(new_r) - colexclu != to_jsonb(old_r) - colexclu THEN
+     RETURN False;
+   ELSE
+     RETURN True;
+   END IF;
+
 END;
 $$;
 --
@@ -106,11 +133,10 @@ BEGIN
 
    ALTER TRIGGER pg_upless_%s_trg ON %s.%s DEPENDS ON EXTENSION pg_upless;
    ', table_source, schema_source, table_source, table_source, schema_source, table_source);
-
 END;
 $$;
 --
---
+-- The table that will contains the statistics
 --
 CREATE TABLE IF NOT EXISTS pg_upless_stats (
   relnamespace name NOT NULL,
@@ -119,10 +145,31 @@ CREATE TABLE IF NOT EXISTS pg_upless_stats (
   useless      bigint,
   PRIMARY key (relnamespace, relname)
 );
-
+--
+-- The table that keep the timestamp when we start to collect the data
+--
 CREATE TABLE IF NOT EXISTS pg_upless_start_time (
   relnamespace name NOT NULL,
   relname      name NOT NULL,
   start_time   timestamp with time zone,
   PRIMARY key (relnamespace, relname)
+);
+--
+-- The table to configure the column to be excluded per table source
+--
+CREATE TABLE IF NOT EXISTS pg_upless_column_exclusion_table (
+  relnamespace name NOT NULL,
+  relname      name NOT NULL,
+  colname      name NOT NULL,
+  defined_at timestamp with time zone DEFAULT current_timestamp,
+  defined_by text DEFAULT current_user,
+  PRIMARY key (relnamespace, relname, colname)
+);
+--
+-- The table to configure the column to be excluded for all tables
+--
+CREATE TABLE IF NOT EXISTS pg_upless_column_exclusion (
+  colname name NOT NULL PRIMARY KEY,
+  defined_at timestamp with time zone DEFAULT current_timestamp,
+  defined_by text DEFAULT current_user
 );
