dda6f6b70db60fe2799fadade373dae4ac19ac7e
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-12-16 11:52:53 +0100

    Exclude some columns when comparing records

---
+++ test/sql/function_test.sql
@@ -0,0 +1,32 @@
+BEGIN;
+
+-- Define the number of tests to run
+SELECT plan(6);
+
+SELECT has_function('public'::name,
+                    'pg_upless_start'::name,
+                    ARRAY['name']);
+
+SELECT has_function('public'::name,
+                    'pg_upless_start'::name,
+                    ARRAY['name','name']);
+
+SELECT has_function('public'::name,
+                    'pg_upless_stop'::name,
+                    ARRAY['name','name']);
+
+SELECT has_function('public'::name,
+                    'pg_upless_create_trigger'::name,
+                    ARRAY['name','name']);
+
+SELECT has_function('public'::name,
+                    'pg_upless_compare_record'::name,
+                    ARRAY['record','record']);
+
+SELECT has_function('public'::name,
+                    'pg_upless_stats_trg'::name);
+
+
+SELECT * FROM finish();
+-- Always end unittest with a rollback
+ROLLBACK;
--- test/sql/table_test.sql
+++ test/sql/table_test.sql
@@ -1,13 +1,16 @@
 BEGIN;
 
-
-
 -- Define the number of tests to run
-SELECT plan(2);
+SELECT plan(4);
 
-SELECT has_table('public'::name, 'pg_upless_stats'::name, 'Table pg_upless_stats exists');
+SELECT has_table('public'::name, 'pg_upless_stats'::name);
 
-SELECT has_table('public'::name, 'pg_upless_start_time'::name, 'Table pg_upless_start_time exists');
+SELECT has_table('public'::name, 'pg_upless_start_time'::name);
+
+SELECT has_table('public'::name, 'pg_upless_column_exclusion_table'::name);
+
+SELECT has_table('public'::name, 'pg_upless_column_exclusion'::name);
+
 
 SELECT * FROM finish();
 -- Always end unittest with a rollback
